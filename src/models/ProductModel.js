ed66094b3e3841736078016c4836ad8c2fc61986
Holmkvists <adamholmkvists@gmail.com>
2023-01-20 03:04:30 +0100

    removed npm package

--- src/models/ProductModel.js
+++ src/models/ProductModel.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 const productSchema = mongoose.Schema({
   name: { type: String, required: true },
   imgSrc: { type: String, required: true },
-  sizes: [{ type: String, required: false }],
+  sizes: [{ type: String, required: true }],
   price: { type: String, required: true },
-  material: { type: String, required: false },
+  material: { type: String, required: true },
 });
 
 const ProductModel = mongoose.model("products", productSchema);
